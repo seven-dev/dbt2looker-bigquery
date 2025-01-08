3c7c53ce01116ccd09272137f00bb8ef79fc4479
carlos-mtns <carlos.martins@devoteam.com>
2025-01-08 11:53:57 +0100

    added hidden_dimensions parameter to set all lkml with hidden=yes paramenter

--- dbt2looker_bigquery/models.py
+++ dbt2looker_bigquery/models.py
@@ -247,3 +247,6 @@ class DbtManifest(BaseModel):
     nodes: Dict[str, Union[DbtModel, DbtNode]]
     metadata: DbtManifestMetadata
     exposures : Dict[str, DbtExposure]
+
+class HiddenDimension():
+    is_hidden = False    
